17f5fd9b46c869904f75609f19273c128b67f55b
Leon Shimizu <leon@Kamis-MacBook-Air-2.local>
2026-08-22 15:31:25 +1000

    Add happy/sad paths for show, update, and destroy - and add validation for name on task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,88 @@
 class TasksController < ApplicationController
   # CRUD
-  # Create Read (index and show) Update Destroy
+  # Create
+  # Read - Index and Show
+  # Update
+  # Destroy
 
+  # READ - ALL TASKS
   def index
-    task = Task.all
+    tasks = Task.all
 
-    render json: task
+    render json: tasks
   end
 
+
+  # CREATE
   def create
-    if params[:name]
-      task = Task.create(
+    task = Task.new(
+      name: params[:name],
+      description: params[:description]
+    )
+
+    if task.save
+      # HAPPY PATH
+      render json: task, status: :created
+    else
+      # SAD PATH
+      render json: { errors: task.errors.full_messages },
+        status: :unprocessable_entity
+    end
+  end
+
+
+  # READ - ONE TASK
+  def show
+    task = Task.find_by(id: params[:id])
+
+    if task
+      # HAPPY PATH
+      render json: task
+    else
+      # SAD PATH
+      render json: { message: "Task doesn't exist." },
+        status: :not_found
+    end
+  end
+
+
+  # UPDATE
+  def update
+    task = Task.find_by(id: params[:id])
+
+    if task
+      if task.update(
         name: params[:name],
         description: params[:description]
       )
-
-      render json: task, status: :created
+        # HAPPY PATH
+        render json: task
+      else
+        # SAD PATH - Task exists, but update failed
+        render json: { errors: task.errors.full_messages },
+          status: :unprocessable_entity
+      end
     else
-      render json: {message: "Please include the name field."}, status: :not_acceptable
+      # SAD PATH - Task doesn't exist
+      render json: { message: "Task doesn't exist." },
+        status: :not_found
+    end
+  end
+
+
+  # DESTROY
+  def destroy
+    task = Task.find_by(id: params[:id])
+
+    if task
+      # HAPPY PATH
+      task.destroy
+
+      render json: { message: "Task deleted." }
+    else
+      # SAD PATH
+      render json: { message: "Task doesn't exist." },
+        status: :not_found
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "/tasks" => "tasks#index"
-
   post "/tasks" => "tasks#create"
+  get "/tasks/:id" => "tasks#show"
+  put "/tasks/:id" => "tasks#update"
+  delete "/tasks/:id" => "tasks#destroy"
 end
